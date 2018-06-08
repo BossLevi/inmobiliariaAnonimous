df64346d1179b3dd462628b1f06f19790f58be8d
AngelRA <acherfun99@gmail.com>
2018-06-07 23:34:38 -0400

    Buscador terminado con js, revisenlo... dudas me dicen al wtsp

--- InmobiliariaV2.0/js/llenardatosdt.js
+++ InmobiliariaV2.0/js/llenardatosdt.js
@@ -42,7 +42,7 @@
             }            
         }     
         
-        alert(cantarray/4);
+        
         
         for(var i=1;i<(cantarray/4);i++)
         {
@@ -51,7 +51,7 @@
             }
 
             if((cantarray/4)>=3){
-                alert(i);
+                
             document.getElementById("tabla").deleteRow(1);
             }
         }
@@ -71,39 +71,11 @@
                 var fila = document.createElement("tr");
                 
             }
-        }
-       // document.getElementById("tabla").deleteRow(1);
-        /*var fila = document.createElement("tr");
-        var columnatipo= document.createElement("td");
-        var columnapro= document.createElement("td");
-        var columnazona= document.createElement("td");
-        var columnaprice= document.createElement("td");
-
-
-        var contenidotipo = document.createTextNode(tip);
-        var contenidoprov = document.createTextNode(pro);
-        var contenidozona = document.createTextNode(zona);
-        var contenidoprice = document.createTextNode(precio+"$");
-        //agregamos una columna
-        columnatipo.appendChild(contenidotipo);
-        columnapro.appendChild(contenidoprov);
-        columnazona.appendChild(contenidozona);
-        columnaprice.appendChild(contenidoprice);
-        //agregamos la columna a una fila
-        fila.appendChild(columnatipo);
-        fila.appendChild(columnapro);
-        fila.appendChild(columnazona);
-        fila.appendChild(columnaprice);
-        
-
-        //agregamos la fila a la tabla        
-        tb.appendChild(fila);  */     
+        }      
         
     };
 
-    
-
-    var buscartarea=function(){        
+    var mostrarTodo=function(){
         var idtipo = document.getElementById("sltipo");
         var tip = idtipo.options[idtipo.selectedIndex].value;
         
@@ -115,10 +87,41 @@
         
         var precio = document.getElementById("txtprice").value
        
-        var tb = document.getElementById("tabla");        
+        var tb = document.getElementById("tabla");
 
-        var fila = document.createElement("tr");
+        var array = new Array(tip,pro,zona,precio);
         
+        var cantarray= aux.length;
+        for(var i=0; i< array.length ;i++)
+        {
+            
+            if(cantarray==0)
+            {
+                aux[i]=array[i];                
+                
+            }
+            else{
+                aux[cantarray]=array[i];
+                cantarray++;                
+               
+            }            
+        }     
+        
+        
+        
+        for(var i=1;i<(cantarray/4);i++)
+        {
+            if((cantarray/4)==2){
+            document.getElementById("tabla").deleteRow(1);
+            }
+
+            if((cantarray/4)>=3){
+                
+            document.getElementById("tabla").deleteRow(1);
+            }
+        }
+        
+        var fila = document.createElement("tr");        
 
         for(var i =0;i<aux.length;i++)
         {
@@ -133,7 +136,82 @@
                 var fila = document.createElement("tr");
                 
             }
-        }
+        }  
+        document.getElementById("tabla").deleteRow((aux.length/4));
+    };
+    
+
+    var buscartarea=function(){       
+      
+        
+       		var tableReg = document.getElementById('tabla');
+
+			var idtipo = document.getElementById('sltipo');
+            var tip = idtipo.options[idtipo.selectedIndex].value.toLowerCase();
+            
+            var idprovincia=document.getElementById('slprov');
+            var pro = idprovincia.options[idprovincia.selectedIndex].value.toLowerCase();
+            
+            var idzona=document.getElementById('slzona');
+            var zona = idzona.options[idzona.selectedIndex].value.toLowerCase();
+            
+            var precio=document.getElementById('txtprice').value.toLowerCase();
+                        
+			var cellsOfRow="";
+
+			var found=false;
+
+			var compareWith="";
+
+            
+
+			// Recorremos todas las filas con contenido de la tabla
+
+			for (var i = 1; i < tableReg.rows.length; i++)
+
+			{
+
+				cellsOfRow = tableReg.rows[i].getElementsByTagName('td');
+
+				found = false;
+
+				// Recorremos todas las celdas
+
+				for (var j = 0; j < cellsOfRow.length && !found; j++)
+
+				{
+
+					compareWith = cellsOfRow[j].innerHTML.toLowerCase();
+
+					// Buscamos el texto en el contenido de la celda
+
+					if (tip.length == 0 || (compareWith.indexOf(tip) > -1) || pro.length == 0 || (compareWith.indexOf(pro) > -1)|| zona.length == 0 || (compareWith.indexOf(zona) > -1)|| precio.length == 0 || (compareWith.indexOf(precio) > -1))
+
+					{
+
+						found = true;
+
+					}
+
+				}
+
+				if(found)
+
+				{
+
+					tableReg.rows[i].style.display = '';
+
+				} else {
+
+					// si no ha encontrado ninguna coincidencia, esconde la
+
+					// fila de la tabla
+
+					tableReg.rows[i].style.display = 'none';
+
+				}
+
+			}
     };
     //eventos        
     
